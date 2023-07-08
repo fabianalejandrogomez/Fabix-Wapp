85d8683976be7cddf0ef93a62b3d612b2775e02f
Fabián Alejandro Gómez Gancedo <52356912+fabianalejandrogomez@users.noreply.github.com>
2023-07-08 17:06:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+import openai
+
+
+
 app=FastAPI()
 
 @app.get("/")
@@ -8,4 +12,19 @@ async def root():
 @app.get("/test")
 async def root():
     return {"message":"this is test"}
+
+@app.get("/aplicar_openai", status_code=status.HTTP_200_OK, tags=["OpenAI"])
+def get_all_fruits(pregunta: OPenAISchema):
+
+    openai.api_key = "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"
+
+    response = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt=pregunta.question,
+    max_tokens=60
+    )
+    message = response.choices[0].text.strip()
+
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"message":message})
+
     
