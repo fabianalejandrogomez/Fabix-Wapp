78c1f7ec433100363de5fe9c511875d97239812a
Fabián Alejandro Gómez Gancedo <52356912+fabianalejandrogomez@users.noreply.github.com>
2023-07-08 17:56:01 -0300

    actualizado

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
 import openai
-
+import os
 
 
 app=FastAPI()
 
+load_dotenv()
+
+openai.api_key = os.getenv('SECRET_KEY')
+
+class Prompt(BaseModel):
+    text: str = Field(min_lenght=10, max_lenght=100)
+
 @app.get("/")
 async def root():
-    return {"message":"Hello World from Smurfcoders"}
+    return {"message":"Hello World from Fabian"}
 
 @app.get("/test")
 async def root():
     return {"message":"this is test"}
 
-@app.get("/aplicar_openai", status_code=status.HTTP_200_OK, tags=["OpenAI"])
-def get_all_fruits(pregunta: OPenAISchema):
-
-    openai.api_key = ""
-
+@app.post('/chat')
+def generate_response(prompt: Prompt):
+    text_lenght = 1000
+    gpt_model = "text-davinci-002"
     response = openai.Completion.create(
-    engine="text-davinci-002",
-    prompt=pregunta.question,
-    max_tokens=60
+        engine=gpt_model,
+        prompt=prompt.text,
+        max_tokens=text_lenght,
+        n=1,
+        stop=None,
+        temperature=0.5
     )
-    message = response.choices[0].text.strip()
-
-    return JSONResponse(status_code=status.HTTP_200_OK, content={"message":message})
+    return response['choices'][0]['text']
 
     
